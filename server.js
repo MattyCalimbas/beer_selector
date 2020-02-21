f2f2ba0c13d56663307f6f2077a424ff5b53009c
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2020-02-20 19:32:33 -0800

    modified server file

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.set('view engine', 'ejs');
 var routes = require('./server/config/routes.js');
 routes(app)
 
-app.listen(8000, function() {
-console.log('listening on port 8000');
-})
+app.listen(process.env.PORT || 8000)
+
+// app.listen(8000, function() {
+// console.log('listening on port 8000');
+// })
